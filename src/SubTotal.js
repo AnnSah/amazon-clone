65fdc89ac714a294734f4e5d04ae55f2d6c5fdf4
Anisa <anissa.sahiiti@gmail.com>
2022-09-07 12:00:11 +0200

    ADD: Amazon

--- src/SubTotal.js
+++ src/SubTotal.js
@@ -13,7 +13,7 @@ function SubTotal() {
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal ({basket.length} items): <strong>{{ value }}</strong>
+                            Subtotal ({basket.length} items): <strong>{``}</strong>
                         </p>
                         <small className="subtotal__gift">
                             <input type="checkbox" /> This order contains a gift
